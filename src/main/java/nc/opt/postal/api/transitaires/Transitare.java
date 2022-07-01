a21f1b83b32dde7e4164c3c7170ef8bf8d54b195
Daniel Santos <dougniel@gmail.com>
2022-07-01 15:58:44 +0200

    feat(api): Critère permettant la recherche par moc clés
    https://github.com/opt-nc/GLIA/issues/685

--- src/main/java/nc/opt/postal/api/transitaires/Transitare.java
+++ src/main/java/nc/opt/postal/api/transitaires/Transitare.java
@@ -1,13 +1,11 @@
 package nc.opt.postal.api.transitaires;
 
-import java.text.Normalizer;
+public record Transitare(String id, String name, String phone, String email, String website, String facebook) {
+    public static record Scored(Transitare transitaire, long score) implements Comparable<Scored> {
 
-public record Transitare(String name, String phone, String email, String website, String facebook) {
-    public String getId() {
-        return Normalizer
-            .normalize(name, Normalizer.Form.NFD)
-            .replaceAll("[\\p{InCombiningDiacriticalMarks}]", "")
-            .replaceAll("\\W+", "_")
-            .toUpperCase();
+        @Override
+        public int compareTo(Scored o) {
+            return Long.compare(this.score, o.score);
+        }
     }
 }
