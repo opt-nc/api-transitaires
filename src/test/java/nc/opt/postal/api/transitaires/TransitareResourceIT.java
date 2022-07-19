63eca4d3d71385aeaf5f4173a7c66ac73b78f89e
Daniel Santos <dougniel@gmail.com>
2022-07-19 11:32:58 +0000

    ci(tests): tests unitaires en natif et non natif

--- src/test/java/nc/opt/postal/api/transitaires/TransitareResourceIT.java
+++ src/test/java/nc/opt/postal/api/transitaires/TransitareResourceIT.java
@@ -1,24 +1,6 @@
 package nc.opt.postal.api.transitaires;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
-import javax.ws.rs.core.MediaType;
-
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class TransitareResourceIT extends TransitareResourceTest {
-
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/transitaires")
-          .then()
-             .statusCode(200)
-             .contentType(MediaType.APPLICATION_JSON)
-             .body("$.size()", is(24), "[0].name", is("EKVF"), "[0].phone", is("27.25.92"));
-    }
 }
